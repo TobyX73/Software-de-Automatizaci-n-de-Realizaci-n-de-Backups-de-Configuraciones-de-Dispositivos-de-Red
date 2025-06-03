6757fb09156024df526799580a5c9ee028e7aa51
Toby <tobybecerra04@gmail.com>
2025-06-03 01:34:04 -0300

    Build: Added spring boot and web dependencies for the proyect

---
+++ BackupApi/BackupApi/src/main/java/com/toby/BackupApi/BackupApiApplication.java
@@ -0,0 +1,13 @@
+package com.toby.BackupApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackupApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackupApiApplication.class, args);
+	}
+
+}
---
+++ BackupApi/BackupApi/src/test/java/com/toby/BackupApi/BackupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toby.BackupApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackupApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
