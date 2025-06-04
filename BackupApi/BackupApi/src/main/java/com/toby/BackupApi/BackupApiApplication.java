3d66187560570b0c23ae8f3771d6e7ee78a29a05
IsaAlegre <isabellaalegre84@gmail.com>
2025-06-04 18:31:57 -0300

    build new dependencies

--- BackupApi/BackupApi/src/main/java/com/toby/BackupApi/BackupApiApplication.java
+++ BackupApi/BackupApi/src/main/java/com/toby/BackupApi/BackupApiApplication.java
@@ -2,12 +2,23 @@ package com.toby.BackupApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
 
 @SpringBootApplication
 public class BackupApiApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		// Asigna a variables del sistema para que Spring las lea
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+
 		SpringApplication.run(BackupApiApplication.class, args);
+
 	}
 
 }
