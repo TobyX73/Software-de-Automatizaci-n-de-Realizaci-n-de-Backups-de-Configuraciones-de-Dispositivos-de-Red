a1378f029b02529edf931983009863cd0fd5095d
IsaAlegre <isabellaalegre84@gmail.com>
2025-06-13 02:48:23 -0300

    SFTP file download method for device backups has been added

--- BackupApi/BackupApi/src/main/java/com/toby/BackupApi/service/SSHService.java
+++ BackupApi/BackupApi/src/main/java/com/toby/BackupApi/service/SSHService.java
@@ -48,4 +48,59 @@ public class SSHService {
 
         return salida.toString();
     }
+
+    public boolean probarConexionSSH(String host, int puerto, String usuario, String password) {
+        try {
+            JSch jsch = new JSch();
+            Session session = jsch.getSession(usuario, host, puerto);
+            session.setPassword(password);
+
+            Properties config = new Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+
+            session.connect(3000); // timeout
+
+            boolean conectado = session.isConnected();
+            session.disconnect();
+
+            return conectado;
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean descargarArchivoSFTP(String host, int puerto, String usuario, String password, String remoteFile, String localFile) {
+        com.jcraft.jsch.Session session = null;
+        com.jcraft.jsch.ChannelSftp sftpChannel = null;
+        try {
+            JSch jsch = new JSch();
+            session = jsch.getSession(usuario, host, puerto);
+            session.setPassword(password);
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+            session.connect();
+
+            sftpChannel = (ChannelSftp) session.openChannel("sftp");
+            sftpChannel.connect();
+
+            java.io.File localDir = new java.io.File(localFile).getParentFile();
+            if (localDir != null && !localDir.exists()) localDir.mkdirs();
+
+            sftpChannel.get(remoteFile, localFile);
+
+            sftpChannel.disconnect();
+            session.disconnect();
+            return true;
+        } catch (Exception e) {
+            System.out.println("[ERROR] Error al descargar archivo SFTP: " + e.getMessage());
+            e.printStackTrace();
+            if (sftpChannel != null && sftpChannel.isConnected()) sftpChannel.disconnect();
+            if (session != null && session.isConnected()) session.disconnect();
+            return false;
+        }
+    }
+
 }
